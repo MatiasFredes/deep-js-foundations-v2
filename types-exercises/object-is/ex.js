1c1d00ff15e720c8870131795c6edfe781c356d4
Matias <mnfredes@gmail.com>
2020-08-12 19:55:40 -0300

    solve types exercise

--- types-exercises/object-is/ex.js
+++ types-exercises/object-is/ex.js
@@ -1,6 +1,21 @@
 // TODO: define polyfill for `Object.is(..)`
 
+if (!Object.is || true) {
+	Object.is = function ObjectIs(val1, val2) { 
+        if(isNotANumber(val1, val2))
+            return true;
+        else if((1 / val1 == Infinity && 1 / val2 == -Infinity) ||
+                (1 / val1 == -Infinity && 1 / val2 == Infinity))
+                return false;
+        return val1 === val2;
+    };
 
+}
+
+function isNotANumber(val1, val2) {
+    return typeof val1 == "number" && typeof val2 =="number" &&
+        isNaN(val1) && isNaN(val2) && val1 !== val2;
+}
 
 // tests:
 console.log(Object.is(42,42) === true);
